215f3956d7cd4cbdc8f3ff37ac4f8bda9a88eb89
alijonizabiri <alijonzabirov20@mail.ru>
2024-04-30 11:33:46 +0500

    fsf

--- obj/Debug/net8.0/DockerComposeDemoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DockerComposeDemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerComposeDemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde3ddc5480c743c84919925aea440bfb40d59db")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerComposeDemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerComposeDemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
